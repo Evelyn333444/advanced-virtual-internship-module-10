1c50fa17bfa2c03f24c939c3c82552b3f01e60e3
Evelyn333444 <brackesb12@gmail.com>
2026-05-10 15:06:37 -0500

    first push

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -1,7 +1,18 @@
 const SideBar = () => {
     return (
         <>
-       
+       <div class="sidebar__toggle--btn">
+                                            <svg stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 15 15" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd" clip-rule="evenodd" d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z" fill="currentColor"></path>
+                                                </svg>
+                                                </div>
+                                                
+                                                
+                                               
+                                                <div class="sidebar__overlay sidebar__overlay--hidden"></div>
+                                                <div class="sidebar sidebar--closed">
+                                                    <div class="sidebar__logo">
+                                                        <img alt="" srcSet="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.1b1c490b.png&amp;w=640&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.1b1c490b.png&amp;w=1080&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.1b1c490b.png&amp;w=1080&amp;q=75" width="495" height="114" decoding="async" data-nimg="1" loading="lazy" style="color:transparent"></img></div>
 <div classNameName="sidebar__wrapper">
                         <div classNameName="sidebar__top">
                             <a classNameName="sidebar__link--wrapper" href="/for-you">
