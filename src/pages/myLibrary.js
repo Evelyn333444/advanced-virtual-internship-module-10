4b693c4ad8ca5150dd7bf4f852ed342b372de45c
Evelyn333444 <brackesb12@gmail.com>
2026-05-10 16:26:01 -0500

    first push

--- src/pages/myLibrary.js
+++ src/pages/myLibrary.js
@@ -4,44 +4,45 @@ const MyLibrary = () => {
     return (
         <>
 <div id="__next">
-                <div class="wrapper">
-                    <div class="search__background">
-                        <div class="search__wrapper">
+                <div classNameName="wrapper">
+                    <div classNameName="search__background">
+                        <div classNameName="search__wrapper">
                             <figure>
                                 <img src="logo" alt=""/>
                                 </figure>
-                                <div class="search__content">
-                                    <div class="search">
-                                        <div class="search__input--wrapper">
-                                            <input class="search__input" placeholder="Search for books" type="text" value=""/>
-                                            <div class="search__icon">
-                                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                <div classNameName="search__content">
+                                    <div classNameName="search">
+                                        <div classNameName="search__input--wrapper">
+                                            <input className="search__input" placeholder="Search for books" type="text" defaultValue=""/>
+                                            <div classNameName="search__icon">
+                                                <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                                                     <path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path>
                                                     </svg>
                                                     </div>
                                                     </div>
                                                     </div>
-                                                    <div class="sidebar__toggle--btn">
-                                                        <svg stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 15 15" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-                                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z" fill="currentColor"></path>
+                                                    <div classNameName="sidebar__toggle--btn">
+                                                        <svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 15 15" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                                                            <path fillRule="evenodd" clipRule="evenodd" d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z" fill="currentColor"></path>
                                                             </svg>
                                                             </div>
                                                             </div>
                                                             </div>
                                                             </div>
-                                                            <div class="sidebar__overlay sidebar__overlay--hidden"></div>
-                                                            <div class="sidebar sidebar--closed">
-                                                                <div class="sidebar__logo">
-                                                                    <img alt="" srcSet="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.1b1c490b.png&amp;w=640&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.1b1c490b.png&amp;w=1080&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.1b1c490b.png&amp;w=1080&amp;q=75" width="495" height="114" decoding="async" data-nimg="1" loading="lazy" style="color:transparent"/>
+                                                            <div classNameName="sidebar__overlay sidebar__overlay--hidden"></div>
+                                                            <div classNameName="sidebar sidebar--closed">
+                                                                <div classNameName="sidebar__logo">
+                                                                    <img alt="" srcSet="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.1b1c490b.png&amp;w=640&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.1b1c490b.png&amp;w=1080&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.1b1c490b.png&amp;w=1080&amp;q=75" width="495" height="114" decoding="async" data-nimg="1" loading="lazy" style={{color:"transparent"}}/>
                                                                     </div>
                                                                     <SideBar/>
                                                                                                                                                                     
-                                                                                                                                                                    <div class="row">
-                                                                                                                                                                        <div class="container">
-                                                                                                                                                                            <div class="settings__login--wrapper">
-                                                                                                                                                                                <img alt="login" srcSet="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogin.e313e580.png&amp;w=1080&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogin.e313e580.png&amp;w=3840&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogin.e313e580.png&amp;w=3840&amp;q=75" width="1033" height="712" decoding="async" data-nimg="1" loading="lazy" style="color: transparent;"/>
-                                                                                                                                                                                <div class="settings__login--text">Log in to your account to see your library.</div>
-                                                                                                                                                                                <button class="btn settings__login--btn">Login</button>
+                                                                                                                                                                    <div classNameName="row">
+                                                                                                                                                                        <div className="container">
+                                                                                                                                                                            <div className="settings__login--wrapper">
+                                                                                                                                                                                <img alt="login" srcSet="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogin.e313e580.png&amp;w=1080&amp;q=75 1x, /_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogin.e313e580.png&amp;w=3840&amp;q=75 2x" src="/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogin.e313e580.png&amp;w=3840&amp;q=75" width="1033" height="712" decoding="async" data-nimg="1" loading="lazy" style={{color: "transparent"}}/>
+                                                                                                                                                                                <div className="settings__login--text">Log in to your account to see your library.</div>
+                                                                                                                                                                                <button className="btn settings__login--btn">Login</button>
+                                                                                                                                                                                </div>
                                                                                                                                                                                 </div>
                                                                                                                                                                                 </div>
                                                                                                                                                                                 </div>
