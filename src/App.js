49b1d00fcca8ea008abf0d9c16ae259b815f3e64
Evelyn333444 <brackesb12@gmail.com>
2026-05-10 14:27:06 -0500

    first push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import MyLibrary from './pages/myLibrary';
+import Settings from './pages/settings';
+import ForYou from './pages/forYou';
 
-<div className="App">
-  <Routes>
-    <Route path="/library" element={<MyLibrary />} />
-    <Route path="/settings" element={<Settings />} />
-    <Route path="/" element={<Home />} />
-    <Route path="/forYou" element={<ForYou />} />
-  </Routes>
-</div>
+const App = () => {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/library" element={<MyLibrary />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/forYou" element={<ForYou />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
