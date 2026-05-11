557c3ad99c8f8306b6fd8a47bcb2ee6366231b13
Evelyn333444 <brackesb12@gmail.com>
2026-05-11 12:51:04 -0500

    first push

--- src/components/loginToggle.js
+++ src/components/loginToggle.js
@@ -14,9 +14,12 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 const LoginToggle = ({ onClose }) => {
-    const [ showdontAccount, setdontAccount ] = useState(false)
+    const [ showLogin, setShowLogin ] = useState(false)
     return (
         <>
+
+<div className="auth__wrapper">
+    <div className="auth">
         <div className="auth__content">
             <div className="auth__title">Log in to Summarist</div>
             <button className="btn guest__btn--wrapper">
@@ -46,12 +49,14 @@ const LoginToggle = ({ onClose }) => {
                                             </button>
                                             </form>
                                             </div>
-                                            <div className="auth__forgot--password">Forgot your password?</div>
-                                            <button className="auth__switch--btn" onClick={() => setdontAccount(true)}>Don't have an account?</button>
-                                            { showdontAccount && <LoginToggle onClose={() => setdontAccount(false)} />}
-                                            <div className="auth__close--btn" onClick={onClose}>
+                                            <div className="auth__forgot--password" onClick={() => setForgotPassword(true)}>Forgot your password?</div>
+                                            <button className="auth__switch--btn">Don't have an account?</button>
+                                            <div className="auth__close--btn" onClick={() => setShowLogin(true)}>
+                                                { showLogin && <LoginToggle onClose={() => setShowLogin(false)} />}
                                                 <svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z" fill="currentColor"></path>
                                                 </svg>
+                                                </div>
+                                                </div>
                                                 </div>
                                                 
 </>
