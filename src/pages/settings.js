e4c373c57ae4fc819517f76372f2cb0ca4169c12
Evelyn333444 <brackesb12@gmail.com>
2026-05-15 11:13:57 -0500

    first push

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,30 +1,40 @@
-import React from 'react';
 import SideBar from '../components/sideBar';
 import Search from '../components/search';
+import Footer from '../components/footer'
+import { useState } from 'react'
 
-const Settings = ({ handleLogout }) => {
+const Settings = () => {
+    const [showUpgrade, setUpgrade] = useState(false);
   return (
+    <>
     <div id="__next">
-      <div className="wrapper">
-        <Search />
-        <SideBar />
-        <div className="row">
-          <div className="container">
-            <div className="settings__wrapper">
-              <h1 className="settings__title">Settings</h1>
-              <div className="settings__content">
-                <button className="btn" onClick={handleLogout}>
-                  <span>Logout</span>
-                </button>
-              </div>
-            </div>
-          </div>
+    <div className="wrapper">
+    <Search />
+    <Sidebar />
         </div>
-      </div>
-    </div>
-  );
-};
+        </div>
+        <div className="container">
+        <div className="row">
+        <div className="section__title page__title">Settings</div>
+        <div className="setting__content">
+        <div className="settings__sub--title">Your Subscription plan</div>
+        <div className="settings__text" onClick={() => setUpgrade (true)}>Premium</div>
+        <a className="btn settings__upgrade--btn" href="/choose-plan">Upgrade to Premium</a>
+        </div>
+        <div className="setting__content">
+        <div className="settings__sub--title">Email</div>
+        <div className="settings__text">brackesb12@gmail.com</div>
+        //FIX THIS, ITS NOT SUPPOSED TO BE HARDCODED
+        </div>
+        </div>
+        </div>
+    </>
+  )
+}
 
-export default Settings;
+export default Settings
 
-//This page is when you have not logged in and the settings page just has an image and a button that says login
+//This page is when you are logged in and there is a green button that says 'Upgrade'
+//<button className="btn" onClick={handleLogout}>
+//                  <span>Logout</span>
+ //               </button>
