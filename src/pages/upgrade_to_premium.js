96bb18e88c1a132c540b340bca0439cbcdfb7ff3
Evelyn333444 <brackesb12@gmail.com>
2026-05-16 18:52:03 -0500

    first push

--- src/pages/upgrade_to_premium.js
+++ src/pages/upgrade_to_premium.js
@@ -47,6 +47,7 @@ const UpgradeToPremium = () => {
     </div>
     </div>
     <div className="section__title">Choose the plan that fits you</div>
+    <div className="plan__cards--wrapper">
     <div className="plan__card plan__card--active">
     <div className="plan__card--circle">
         <div className="plan__card--dot"></div>
@@ -57,15 +58,13 @@ const UpgradeToPremium = () => {
     <div className="plan__card--text">7-day free trial included</div>
     </div>
     </div>
-    <div className="plan__card--separator">
-        <div className="plan__separator">or</div>
-    </div>
     <div className="plan__card ">
     <div className="plan__card--circle"></div>
     <div className="plan__card--content">
     <div className="plan__card--title">Premium Monthly</div>
     <div className="plan__card--price">$9.99/month</div>
     <div className="plan__card--text">No trial included</div>
+    </div>
     </div>
     </div>
     <div className="plan__card--cta">
