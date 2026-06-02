07aa4af00c6cc70d132719e0c2f772818009eb62
Evelyn333444 <brackesb12@gmail.com>
2026-06-02 11:06:43 -0500

    first push

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,22 +4,40 @@ import { useState } from 'react'
 
 const Nav = () => {
     const [ showLogin, setShowLogin ] = useState(false)
+    const [ menuOpen, setMenuOpen ] = useState(false)
+
+    const openLogin = () => {
+        setShowLogin(true)
+        setMenuOpen(false)
+    }
+
     return (
         <>
-<div className="nav__wrapper">
-                        <figure class="nav__img--mask">
-                        <img src={logo} alt="logo" />
-                        </figure>
-                        <ul className="nav__list--wrapper">
-                            <li className="nav__list nav__list--login" onClick={() => setShowLogin(true)}>
-                            Login
-                            </li>
-                            <li className="nav__list nav__list--mobile">About</li>
-                            <li className="nav__list nav__list--mobile">Contact</li>
-                            <li className="nav__list nav__list--mobile">Help</li>
-                        </ul>
-                        { showLogin && <LoginToggle onClose={() => setShowLogin(false)} />}
-                    </div>
+            <div className="nav__wrapper">
+                <figure className="nav__img--mask">
+                    <img src={logo} alt="logo" />
+                </figure>
+                <button
+                    type="button"
+                    className={`nav__burger ${menuOpen ? 'nav__burger--open' : ''}`}
+                    aria-label={menuOpen ? 'Close menu' : 'Open menu'}
+                    aria-expanded={menuOpen}
+                    onClick={() => setMenuOpen((open) => !open)}
+                >
+                    <span className="nav__burger-line" />
+                    <span className="nav__burger-line" />
+                    <span className="nav__burger-line" />
+                </button>
+                <ul className={`nav__list--wrapper ${menuOpen ? 'nav__list--wrapper--open' : ''}`}>
+                    <li className="nav__list nav__list--login" onClick={openLogin}>
+                        Login
+                    </li>
+                    <li className="nav__list nav__list--mobile">About</li>
+                    <li className="nav__list nav__list--mobile">Contact</li>
+                    <li className="nav__list nav__list--mobile">Help</li>
+                </ul>
+                { showLogin && <LoginToggle onClose={() => setShowLogin(false)} /> }
+            </div>
         </>
     )
 }
